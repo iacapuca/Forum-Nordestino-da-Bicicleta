71bdec209e4dd23b1edcdc88a8241c8101c5ca29
Pedro Luiz Paes Barreto dos Santos <iacapuca@gmail.com>
2015-06-25 12:31:09 -0300

    Favicon

--- js/scripts.js
+++ js/scripts.js
@@ -1,21 +1,14 @@
 // Smooth Scroll to internal links
 
 $('.smooth-scroll').smoothScroll({
-  speed: 800,
-  offset: -68
+    speed: 800,
+    offset: -68
 });
 
 //Easy Tabs
 $('#tab-container').easytabs({
-	animate: true,
-	animationSpeed: 1000,
+    animate: true,
+    animationSpeed: 1000,
 });
 
 //Dropwdown
-$(".navbar-collapse a").click(function() {
-    $(this).closest(".navbar-collapse collapse").prev().dropdown("toggle");
-});
-
-$('.navbar-collapse collapse in').find("a").not('.navbar-toggle').on("click",function(e){
-    e.stopImmediatePropagation();
-});
