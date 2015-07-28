8bd2558d55ab94cb60f822f16f5844d24bcc8071
Pedro <iacapuca@gmail.com>
2015-07-28 18:31:52 -0300

    Mudança no .js e Finalização do Panel

--- js/scripts.js
+++ js/scripts.js
@@ -49,3 +49,10 @@ $('#submit').click(function () {
 });
 
 
+$(document).on('click','.navbar-collapse.in',function(e) {
+
+    if( $(e.target).is('a') && ( $(e.target).attr('class') != 'dropdown-toggle' ) ) {
+        $(this).collapse('hide');
+    }
+
+});
